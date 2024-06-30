dedf8bb7d69ecca89529d7bad57c14c217a84776
Sajid Jaber <sajidjaber@yahoo.com>
2024-06-30 12:33:59 -0500

    fixed the type error

--- src/app/(pages)/(public)/(portfolio)/CTA.tsx
+++ src/app/(pages)/(public)/(portfolio)/CTA.tsx
@@ -1,20 +1,25 @@
 'use client'
+
 import Link from 'next/link'
 import * as yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
-import { useForm } from 'react-hook-form'
+import { useForm, SubmitHandler } from 'react-hook-form'
 import { FormInput, FormTextArea } from '@/components'
 import { useEffect, useRef, useState } from 'react'
-import emailjs from "@emailjs/browser";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css"; // Import toastify CSS
+import emailjs from "@emailjs/browser"
+import { ToastContainer, toast } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+
+interface FormData {
+  subject: string;
+  name: string;
+  email: string;
+  message: string;
+}
 
 const CTA = () => {
-
-
-
-  const form = useRef(null);
-  const [formData, setFormData] = useState({
+  const form = useRef<HTMLFormElement | null>(null);
+  const [formData, setFormData] = useState<FormData>({
     subject: "",
     name: "",
     email: "",
@@ -25,51 +30,44 @@ const CTA = () => {
     toast.success("Email sent successfully!", {});
   };
 
-  const sendEmail = (e:React.ChangeEvent<any> ) => {
-    e.preventDefault();
-
-    emailjs
-      .sendForm(
-        process.env.NEXT_PUBLIC_YOUR_SERVICE_ID,
-        process.env.NEXT_PUBLIC_YOUR_TEMPLATE_ID,
-        form.current,
-        process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
-      )
-      .then(
-        (result) => {
-          showToastMessage(); // Call showToastMessage when email is sent successfully
-          setFormData({
-            subject: "",
-            email: "",
-            message: "",
-            name: "",
-          });
-        },
-        (error) => {
-          toast.error("Failed to send email: " + error.text, {});
-        }
-      );
+  const sendEmail: SubmitHandler<FormData> = (data) => {
+    emailjs.sendForm(
+      process.env.NEXT_PUBLIC_YOUR_SERVICE_ID!,
+      process.env.NEXT_PUBLIC_YOUR_TEMPLATE_ID!,
+      form.current!,
+      process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!
+    )
+    .then(
+      (result) => {
+        showToastMessage();
+        setFormData({
+          subject: "",
+          email: "",
+          message: "",
+          name: "",
+        });
+      },
+      (error) => {
+        toast.error("Failed to send email: " + error.text, {});
+      }
+    );
   };
 
-  const handleChange = (e:React.ChangeEvent<any>) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
-
   const schemaResolver = yup.object().shape({
     name: yup.string().required('Please enter name'),
     subject: yup.string().required('Please enter subject'),
-    email: yup
-      .string()
-      .required('Please enter email')
-      .email('Please enter valid email'),
+    email: yup.string().required('Please enter email').email('Please enter valid email'),
     message: yup.string().required('Please enter message'),
-  })
+  });
 
-  const { control, handleSubmit } = useForm({
+  const { control, handleSubmit } = useForm<FormData>({
     resolver: yupResolver(schemaResolver),
-  })
+  });
 
   return (
     <section className="pt-20">
@@ -86,62 +84,13 @@ const CTA = () => {
             <p className="text-slate-500 mt-12">Email me at</p>
             <h4>
               <Link href="mailto:sajidjaber@yahoo.com" className="text-lg font-semibold text-slate-600">
-              sajidjaber@yahoo.com
-                            </Link>
+                sajidjaber@yahoo.com
+              </Link>
             </h4>
             <div className="mt-12">
               <div className="flex flex-col gap-3">
                 <h5 className="text-slate-400">Social</h5>
                 <div className="flex gap-5">
-                  {/* <div>
-                    <Link href="">
-                      <svg
-                        className="w-7 h-7 text-slate-500"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth={2}
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      >
-                        <circle cx={12} cy={12} r={10} />
-                        <path d="M8.56 2.75c4.37 6.03 6.02 9.42 8.03 17.72m2.54-15.38c-3.72 4.35-8.94 5.66-16.88 5.85m19.5 1.9c-3.5-.93-6.63-.82-8.94 0-2.58.92-5.01 2.86-7.44 6.32" />
-                      </svg>
-                    </Link>
-                  </div> */}
-                  {/* <div>
-                    <Link href="">
-                      <svg
-                        className="w-7 h-7 text-slate-500"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth={2}
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      >
-                        <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
-                      </svg>
-                    </Link>
-                  </div> */}
-                  {/* <div>
-                    <Link href="">
-                      <svg
-                        className="w-7 h-7 text-slate-500"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth={2}
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      >
-                        <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" />
-                      </svg>
-                    </Link>
-                  </div> */}
                   <div>
                     <Link href="https://www.linkedin.com/in/sajidjaber/">
                       <svg
@@ -160,45 +109,18 @@ const CTA = () => {
                       </svg>
                     </Link>
                   </div>
-                  {/* <div>
-                    <Link href="">
-                      <svg
-                        className="w-7 h-7 text-slate-500"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth={2}
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      >
-                        <rect
-                          x={2}
-                          y={2}
-                          width={20}
-                          height={20}
-                          rx={5}
-                          ry={5}
-                        />
-                        <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
-                        <line x1="17.5" y1="6.5" x2="17.51" y2="6.5" />
-                      </svg>
-                    </Link>
-                  </div> */}
                 </div>
               </div>
             </div>
           </div>
 
           <div data-aos="fade-up" data-aos-duration={900}>
-            <form     
-            ref={form}           
-             onSubmit={sendEmail}>
+            <form ref={form} onSubmit={handleSubmit(sendEmail)}>
               <FormInput
                 control={control}
                 type="text"
                 name="name"
-                id='name'
+                id="name"
                 value={formData.name}
                 onChange={handleChange}
                 className="w-full text-gray-700 border border-slate-200 rounded py-3 px-4 leading-tight focus:outline-none"
@@ -209,7 +131,7 @@ const CTA = () => {
                 control={control}
                 type="email"
                 name="email"
-                id='email'
+                id="email"
                 value={formData.email}
                 onChange={handleChange}
                 className="w-full text-gray-700 border border-slate-200 rounded py-3 px-4 leading-tight focus:outline-none"
@@ -220,7 +142,7 @@ const CTA = () => {
                 control={control}
                 type="text"
                 name="subject"
-                id='subject'
+                id="subject"
                 value={formData.subject}
                 onChange={handleChange}
                 className="w-full text-gray-700 border border-slate-200 rounded py-3 px-4 leading-tight focus:outline-none"
@@ -230,7 +152,7 @@ const CTA = () => {
               <FormTextArea
                 control={control}
                 name="message"
-                id='message'
+                id="message"
                 value={formData.message}
                 onChange={handleChange}
                 className="w-full text-gray-700 border border-slate-200 rounded py-3 px-4 leading-tight focus:outline-none"
@@ -247,21 +169,20 @@ const CTA = () => {
                 </button>
               </div>
               <ToastContainer
-                  style={{ position: "relative" }}
-                  position="bottom-center"
-                  autoClose={5000}
-                  hideProgressBar
-                  newestOnTop={false}
-                  closeOnClick
-                  rtl={false}
-                  pauseOnFocusLoss
-                  draggable
-                  pauseOnHover
-                  theme="dark"
-                  transition="Bounce"
-                />
+                style={{ position: "relative" }}
+                position="bottom-center"
+                autoClose={5000}
+                hideProgressBar
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="dark"
+                transition="Bounce"
+              />
             </form>
-            
           </div>
         </div>
         <hr className="mt-10" />
@@ -270,4 +191,4 @@ const CTA = () => {
   )
 }
 
-export default CTA
+export default CTA;
